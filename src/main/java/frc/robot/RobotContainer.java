4ee18d37c1e00862544d5d48c3851dcbdade1c27
Meh243 <120067923+Meh243@users.noreply.github.com>
2023-10-25 21:08:27 -0400

    rookies added code for shooter subsystem and command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
     
     // TODO: create new private subsystems (indexer and shooter)
+    private Indexer indexer = new Indexer();
+    private Shooter shooter = new Shooter();
 
     public final XboxController driver = new XboxController(JoystickConstants.Ports.COPILOT);
 
@@ -19,8 +21,10 @@ public class RobotContainer {
 
     private void configureBindings() {
         // TODO: map the A button to the Shoot command
-
+        new Trigger(driver::getAButton).whileTrue(new Shoot(shooter, indexer)); 
+        
         // TODO: set indexer default command to run the indexer based on the triggers
+        
     }
 
     public Command getAutonomousCommand() { return null; }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,17 +1,28 @@
 package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.*;
 
 public class Shooter extends SubsystemBase {
 
     // TODO: create shooterMotor of type Victor (public final)
+    public final Victor shooterMotor;
 
     public Shooter() {
-        // TODO: set shooterMotor to a new Victor (use ID from Constants)
-        // TODO: register the subsystem
+        shooterMotor = new Victor(RobotMap.SHOOTER);
+    
+        CommandScheduler.getInstance().registerSubsystem(this);
     }
 
     // TODO: create the setPower method that takes in a double power and sets the power to motor using shooterMotor.set(...);
+    public void setPower(double power) {
+        shooterMotor.set(power);
+    }
 
     // TODO: create the stop method that has no parameters and calls setPower with parameter of 0
+    public void stop(){
+        setPower(0d);
+    }
+
 }
